90b620f83c0ee7c6e3ea2c9705fa133f09cc91e8
therek3r <kennerya@oregonstate.edu>
2020-03-30 21:28:28 -0700

    .

--- shamir/node_register.py
+++ shamir/node_register.py
@@ -4,6 +4,7 @@ import sys
 import aes_crypt
 import rsa_encrypt
 import hashlib
+import base64
 
 class Host():
     def __init__(self):
@@ -11,8 +12,7 @@ class Host():
         self.port = 13337
 
 def register(host):
-    payload = "imup:" 
-    print(hashlib.sha256(rsa_encrypt.get_pub_key().exportKey("PEM")).digest())
+    payload = "imup:" + str(base64.b64encode(hashlib.sha256(rsa_encrypt.get_pub_key().exportKey("PEM")).digest()), 'ascii')
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
         s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
         s.sendto(aes_crypt.aes_enc(rsa_encrypt.get_pub_key_auth(), payload), ((host.host, host.port)))
