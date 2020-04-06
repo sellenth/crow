290361ffe39b97976a80a7f6a7399f7570c337bb
therek3r <kennerya@oregonstate.edu>
2020-04-05 19:56:54 -0700

    .

--- shamir/node_register.py
+++ shamir/node_register.py
@@ -22,6 +22,7 @@ def challenge(payload):
     print("challenging")
     host = Host()
     k = str(base64.b64encode(hashlib.sha256(rsa_encrypt.get_pub_key().exportKey("PEM")).digest()), 'ascii')
+    print(k)
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
         s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
         s.sendto(aes_crypt.aes_enc(rsa_encrypt.get_pub_key_auth(), "who?:" + k), ((host.host, host.port)))
